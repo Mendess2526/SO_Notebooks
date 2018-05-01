329531a396670f7f6a9ac3940af46b214a2a1839
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-01 19:58:37 +0100

    Add readln

--- src/utilities.c
+++ src/utilities.c
@@ -1,21 +1,16 @@
+#include "utilities.h"
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 
 ssize_t readln(int fd, void *buf, ssize_t nbyte){
-	ssize_t size = 0;
-	char c;
-	char *buff = (char *) buf;
-	while(size < nbyte && 1 == read(fd, &c, 1)){
-		if(c == '\0') return size;
-		buff[size++] = c;
-		if(c == '\n') return size;
-	}
-	return size;
+    ssize_t size = 0;
+    char c;
+    char *buff = (char *) buf;
+    while(size < nbyte && 1 == read(fd, &c, 1)){
+        if(c == '\0' || c == '\n') return size;
+        buff[size++] = c;
+    }
+    return size;
 }
-
-//int main(){
-//	char buf[10];
-//	readln(0,buf,10);
-//	printf("%s",buf);
-//}
---
+++ src/utilities.h
@@ -0,0 +1,8 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <unistd.h>
+
+ssize_t readln(int fd, void* buf, ssize_t nbyte);
+
+#endif /* UTILITIES_H */
