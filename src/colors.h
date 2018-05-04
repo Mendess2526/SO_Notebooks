433118632b291e7f874246ccf2a8f2011b83d162
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-05 00:03:16 +0100

    Move colors

---
+++ src/colors.h
@@ -0,0 +1,11 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define BLUE "\033[34m"
+#define PURPLE "\033[35m"
+#define YELLOW "\033[33m"
+#define RESET "\033[0m"
+
+#endif /* COLORS_H */
