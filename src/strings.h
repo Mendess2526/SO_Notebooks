db2b14a72ba92cc549d6eab837cd05d51e0d6478
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-05 00:01:08 +0100

    Add string_append

--- src/strings.h
+++ src/strings.h
@@ -11,6 +11,8 @@ typedef struct string{
 
 void string_init(String* s, char* string, size_t length);
 
+void string_append(String *dest, String src);
+
 void string_free(String s);
 
 #endif /* STRING_H */
