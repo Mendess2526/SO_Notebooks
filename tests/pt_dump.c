68bd3ba4aea01222756ea760a6c5660be27e666b
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-07 10:12:21 +0100

    Change tests

--- tests/pt_dump.c
+++ tests/pt_dump.c
@@ -12,9 +12,11 @@ void parse_and_dump(char *file){
     char* line;
     size_t n;
     while((line = readln(fd, &n)) != NULL){
-        parse_tree_add_line(pt, line, n);
+        printf("%d, ", parse_tree_add_line(pt, line, n));
         free(line);
     }
+    printf("%d", parse_tree_add_line(pt, NULL, 0));
+    printf("\n");
     char** dump = parse_tree_dump(pt);
     int i = 0;
     while(dump[i]){
@@ -26,19 +28,18 @@ void parse_and_dump(char *file){
     parse_tree_destroy(pt);
 }
 
-int main(){
-
-    /* Notebook 0 */
-    printf(GREEN "Notebook 0" RESET "\n");
-    parse_and_dump("notebook0.nb");
-    printf("\n");
-    /* Notebook 1 */
-    printf(GREEN "Notebook 1" RESET "\n");
-    parse_and_dump("notebook1.nb");
-    printf("\n");
-    /* Notebook 2 */
-    printf(GREEN "Notebook 2" RESET "\n");
-    parse_and_dump("notebook2.nb");
-    printf("\n");
+int main(int argc, char** argv){
+    if(argc < 2){
+        printf(GREEN "Notebook 3" RESET "\n");
+        parse_and_dump("notebook3.nb");
+        printf("\n");
+        return 0;
+    }
+    for(int i = 1; i < argc; i++){
+        char filename[30];
+        printf(GREEN "Notebook %s" RESET "\n", argv[i]);
+        sprintf(filename, "notebook %s", argv[i]);
+        parse_and_dump(filename);
+    }
     return 0;
 }
