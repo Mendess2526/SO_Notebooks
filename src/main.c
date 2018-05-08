319ed3c2a8828461c2481ac5e150c6df49cc8902
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-08 22:10:48 +0100

    Implements main skeleton

--- src/main.c
+++ src/main.c
@@ -1,5 +1,41 @@
+#include "parse_tree.h"
+#include "utilities.h"
+
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
 
-int main(){
-    printf("Hello world\n");
+void execBatch(Command c){
+    if(!fork()){
+        //TODO magic
+    }
+}
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        char message[] = "Usage: program notebook.nb";
+        write(1,message, strlen(message));
+        return 1;
+    }
+    //TODO maybe implement read from stdin '-'
+    int fd = open(argv[1],O_RDONLY);
+    char* buff;
+    size_t len;
+    int** pipes; //TODO aloca isto
+    ParseTree pt = parse_tree_create(20);
+    while(NULL != (buff = readln(fd, &len))){
+        int batch = parse_tree_add_line(pt, buff, len);
+        if(batch != -1)
+            execBatch(parse_tree_get_batch(pt, batch));
+    }
+    //TODO read pipes
+
+    close(fd);
+    char** dump = parse_tree_dump(pt);
+    fd = creat(argv[1],O_CREAT);
+    int i = 0;
+    while(dump[i])
+        write(fd,dump,strlen(dump[i++]));
+
+    return 0;
 }
