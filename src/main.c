7ceb0596e18a246d652a19e9d687a4a1117a74f9
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-12 12:03:30 +0100

    Remove and add todo in main

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv){
     if(argc < 2){
-        char message[] = "Usage: program notebook.nb";
+        char message[] = "Usage: ./program <notebook.nb>\n";
         write(1,message, strlen(message));
         return 1;
     }
@@ -17,17 +17,18 @@ int main(int argc, char** argv){
     int fd = open(argv[1],O_RDONLY);
     char* buff;
     size_t len;
-    Pipes mypipes = pipes_create(20); //TODO aloca isto 
+    Pipes mypipes = pipes_create(20);
     ParseTree pt = parse_tree_create(20);
     while(NULL != (buff = readln(fd, &len))){
 
         int batch = parse_tree_add_line(pt, buff, len);
         if(batch != -1){
             pipes_append(mypipes,batch);
-            execBatch(parse_tree_get_batch(pt, batch),pipes_last(mypipes));
+            execBatch(parse_tree_get_batch(pt, batch), pipes_last(mypipes));
         }
     }
-    
+
+    //TODO ler os pipes e prencher os batches correspondentes
 
     close(fd);
     char** dump = parse_tree_dump(pt);
