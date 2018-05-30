c82f68c1879f1d14555cb15498a0d04b10024b40
filipap <a82145@alunos.uminho.pt>
2018-05-30 22:27:27 +0100

    doc utilities not finished

--- src/utilities.h
+++ src/utilities.h
@@ -1,10 +1,22 @@
-#ifndef UTILITIES_H
+	#ifndef UTILITIES_H
 #define UTILITIES_H
 
 #include <unistd.h>
 
+/**
+ * Reads the content from the given file descriptor
+ *
+ * \param fd File descriptor
+ * \param nBytes number of bytes to read
+ */
 char* readLn(int fd, size_t* nBytes);
 
+/**
+ * Separates a string to an array of strings
+ *
+ * \param string String to separate
+ * \param len Length of string
+ */
 char** words(const char* string, size_t len);
 
 size_t int2string(int num, char* string, size_t len);
