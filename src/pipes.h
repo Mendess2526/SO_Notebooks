0382847888f37eddfc4b97bed17821202d9a3d27
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-31 15:06:00 +0100

    Pipe list doc wording

--- src/pipes.h
+++ src/pipes.h
@@ -3,57 +3,57 @@
 
 #include <stdlib.h>
 
-/** 
-* List of pointers to pipes 
+/**
+* A list of pipes
 */
 typedef struct _pipes* Pipes;
 
 /**
  * Creates a list of pipes.
  *
- * \param size Length of list
+ * \param size Initial size of the list
  */
 Pipes pipes_create(size_t size);
 
 /**
- * Appends the element to the list
+ * Appends a new pipe to the list
  *
- * \param p List
+ * \param p The list
  */
 int pipes_append(Pipes p);
 
 /**
- * Get the element from the list at the given index
+ * Get the pipe stored at the given index
  *
- * \param p List
- * \param idx Index to search
- */ 
+ * \param p The list
+ * \param idx The index
+ */
 int* pipes_index(Pipes p, size_t idx);
 
 /**
- * Get the size of the list 
+ * Get the size of the list
  *
- * \param p List
+ * \param p The list
  */
 size_t pipes_len(Pipes p);
 
 /**
- * Get the last element from the list 
+ * Get the last pipe in the list
  *
  * \param p List
  */
 int* pipes_last(Pipes p);
 
 /**
- * Close file descriptors from pipe at the given index 
+ * Close both sides of the pipe at the given index
  *
- * \param p List
- * \param i index 
+ * \param p The list
+ * \param i The index
  */
 void pipes_close(Pipes p, size_t i);
 
 /**
- * Free list
+ * Free the memory used by this list
  *
  * \param p List
  */
