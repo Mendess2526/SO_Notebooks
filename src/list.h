03a32fbb02e52efafc90c7ab5f5e5ce87b3efcfb
Mendess2526 <pedro.mendes.26@gmail.com>
2018-05-31 15:03:03 +0100

    List doc wording

--- src/list.h
+++ src/list.h
@@ -4,102 +4,102 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/** 
-* Index list 
+/**
+* A list of indexes
 */
 typedef struct _list* IdxList;
 
 /**
  * Creates an index list.
  *
- * \param size Length of list
+ * \param size The initial size of the list
  */
 IdxList idx_list_create(size_t size);
 
 /**
- * Appends the element to the list
+ * Appends an element to the list
  *
- * \param l List
- * \param val Value to insert
+ * \param l The list
+ * \param val The value to append
  */
 void idx_list_append(IdxList l, size_t val);
 
 /**
- * Get the element from the list at the given index
+ * Get the element at the given index
  *
- * \param l List
- * \param idx Index to search
+ * \param l The list
+ * \param idx The index
  */
 ssize_t idx_list_index(IdxList l, size_t idx);
 
 /**
- * Get the last element from the list 
+ * Get the last element of the list.
  *
- * \param l List
+ * \param l The list
  */
 ssize_t idx_list_last(IdxList l);
 
 /**
- * Get the size of the list 
+ * Get the size of the list
  *
- * \param l List
+ * \param l The list
  */
 size_t idx_list_len(IdxList l);
 
 /**
- * Get the index where an certain element is
+ * Get the index of the first ocurrence of an element
  *
- * \param l List
- * \param idx Value to search
+ * \param l The list
+ * \param idx The value to search
  */
 ssize_t idx_list_find(IdxList l, size_t idx);
 
 /**
- * Free list
+ * Free the memory used by this list
  *
- * \param l List
+ * \param l The list
  */
 void idx_list_free(IdxList l);
 
-/** 
-* List of pointers 
+/**
+* A list of pointers
 */
 typedef struct _ptr_list* PtrList;
 
 /**
- * Creates a list'of pointers.
+ * Creates a list of pointers.
  *
- * \param size Length of list
+ * \param size The initial size of the list
  */
 PtrList ptr_list_create(size_t size);
 
 /**
  * Appends the element to the list
  *
- * \param l List
- * \param val Value to insert
+ * \param l The list
+ * \param val The value to append
  */
 void ptr_list_append(PtrList l, void* val);
 
 /**
- * Get the element from the list at the given index
+ * Get the pointer stored at the given index
  *
- * \param l List
- * \param idx Index to search
+ * \param l The list
+ * \param idx The index
  */
 void* ptr_list_index(PtrList l, size_t idx);
 
 /**
- * Get the size of the list 
+ * Get the size of the list
  *
- * \param l List
+ * \param l The list
  */
 size_t ptr_list_len(PtrList l);
 
 /**
- * Free list
+ * Free the memory used by this list
  *
- * \param l List
+ * \param l The list
  */
 void ptr_list_free(PtrList l);
 
